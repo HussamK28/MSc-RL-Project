ce51e0b761598995b82ed2c0727eb349c2d2b455
Hussam Khan <HussamK2812@gmail.com>
2026-05-22 12:34:16 +0100

    Update MiniGrid.py

--- MiniGrid.py
+++ MiniGrid.py
@@ -4,3 +4,20 @@ from minigrid.core.grid import Grid
 from minigrid.core.world_object import Door, Key, Goal, Wall
 from minigrid.minigrid_env import MiniGridEnv
 from minigrid.core.mission import MissionSpace
+
+class MiniGrid(MiniGridEnv):
+    def __init__(self, size=8, max_steps=300, **kwargs):
+        instructions = MissionSpace(
+            mission_func=lambda: "You need to find the key before getting to the goal square."
+        )
+
+        super().__init__(
+            mission_space=instructions,
+            width=size,
+            height=size,
+            max_steps=max_steps,
+            see_through_walls=False,
+            agent_view_size=5,
+            **kwargs
+        )
+        
